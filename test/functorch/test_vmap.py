f2dde2e27e72c384f9b46e33f4f13cb33159529c
mingfeima <mingfei.ma@intel.com>
2023-01-23 22:14:05 +0800

    port spmm_reduce to pytorch and optimize it on CPU
    ghstack-source-id: 80adc6da05d1a7714e8f37ea94ed4bfe3be12dd4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83727

--- test/functorch/test_vmap.py
+++ test/functorch/test_vmap.py
@@ -3444,6 +3444,7 @@ class TestVmapOperatorsOpInfo(TestCase):
         xfail('pca_lowrank', ''),  # random operation
         xfail('svd_lowrank', ''),  # random operation
         xfail('sparse.sampled_addmm'),  # sparse
+        xfail('sparse.spmm_reduce'),  # sparse
         xfail("NumpyCubeNotComposableAutogradFunction"),  # Not composable autograd.Function
         skip('_softmax_backward_data'),
         skip('linalg.eigh', ''),  # not unique, see test_linalg_eigh for manual test
